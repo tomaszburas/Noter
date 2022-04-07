499ed56514e12a4ba8d2a29e2d5299648dd4526a
Tomasz Buras <tomaszburas.dev@gmail.com>
2022-04-07 11:08:24 +0200

    fixed heroku

--- server/dist/server/types/NoteRecordEntity/index.js
+++
@@ -1,2 +0,0 @@
-export * from './NoteRecordEntity';
-//# sourceMappingURL=index.js.map
--- server/dist/server/utils/validatePassword.js
+++
@@ -1,4 +0,0 @@
-export const validatePassword = (password) => {
-    return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{5,}$/.test(password);
-};
-//# sourceMappingURL=validatePassword.js.map
