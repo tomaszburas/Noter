569994e3f9da0539116b2f32aa58326512552ac0
Tomasz Buras <tomaszburas.dev@gmail.com>
2022-04-07 20:45:45 +0200

    Edited title - homepage

--- client/src/components/Content/Home/Home.tsx
+++ client/src/components/Content/Home/Home.tsx
@@ -7,7 +7,7 @@ export const Home = () => {
     return (
         <div className={styles.home__container}>
             <p className={styles.home__title}>
-                Notes application with markdown format.
+                Notes app with markdown format.
             </p>
             <img className={styles.home__img} src={homeImg} alt="home img" />
             <Link to="/notes">
