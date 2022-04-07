f263c5c2ce48dc6369a6bb2ad46312d087828a13
Tomasz Buras <tomaszburas.dev@gmail.com>
2022-04-07 11:00:02 +0200

    Fixed heroku

--- server/dist/server.js
+++ server/dist/server.js
@@ -2,25 +2,24 @@ import express, { json } from 'express';
 import 'express-async-errors';
 import passport from 'passport';
 import cookieParser from 'cookie-parser';
-import cors from 'cors';
+import path from 'path';
 import { PORT } from './server/config.js';
 import { Router } from './server/routes/router.js';
 import { handleError } from './server/middleware/errors.js';
 import './server/db/db.js';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
 app.use(json());
-app.use(cors({
-    credentials: true,
-    origin: 'http://localhost:3000',
-}));
 app.use(cookieParser());
 app.use(passport.initialize());
-app.use(express.static('./client/build'));
+app.use(express.static(path.join(__dirname, 'client', 'build')));
 import './server/middleware/passport.js';
 app.use('/', Router);
 app.use(handleError);
 app.get('*', (req, res) => {
-    res.sendFile('./client/build/index.html');
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
 app.listen(PORT, () => console.log('Server has started.'));
 //# sourceMappingURL=server.js.map
