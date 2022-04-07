7fbc081581c30b1cc1fca658e3d7b5822050b298
Tomasz Buras <tomaszburas.dev@gmail.com>
2022-04-07 10:53:22 +0200

    Fixed heroku

--- server.ts
+++ server.ts
@@ -2,6 +2,7 @@ import express, { json } from 'express';
 import 'express-async-errors';
 import passport from 'passport';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 import { PORT } from './server/config.js';
 import { Router } from './server/routes/router.js';
@@ -13,7 +14,7 @@ const app = express();
 app.use(json());
 app.use(cookieParser());
 app.use(passport.initialize());
-app.use(express.static('./client/build'));
+app.use(express.static(path.resolve(__dirname, 'client', 'build')));
 
 import './server/middleware/passport.js';
 
@@ -21,7 +22,7 @@ app.use('/', Router);
 app.use(handleError);
 
 app.get('*', (req, res) => {
-    res.sendFile('./client/build/index.html');
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
 
 app.listen(PORT, () => console.log('Server has started.'));
