84dcac1b9739b2145464588323d2c9ce07c2094a
Tomasz Buras <tomaszburas.dev@gmail.com>
2022-04-17 18:09:16 +0200

    Added Loader to NotesList

--- client/src/components/Content/NotesPage/NotesList/NotesList.tsx
+++ client/src/components/Content/NotesPage/NotesList/NotesList.tsx
@@ -2,17 +2,22 @@ import { NoteElement } from './NoteElement/NoteElement';
 import styles from './NotesList.module.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../../../../redux/store';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { getNotes } from '../../../../redux/features/notes-slice';
+import { Loader } from '../../../Common/Loader/Loader';
 
 export const NotesList = () => {
     const dispatch = useDispatch();
     const { notes } = useSelector((store: RootState) => store.note);
     const { isAuth } = useSelector((store: RootState) => store.user);
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
         dispatch(getNotes());
+        setLoading(true);
     }, [isAuth]);
+
+    if (!loading) return <Loader />;
 
     return (
         <div className={styles.notesContainer}>
